d6672062d52b563499d5665d9c7474b9cac9b7e7
Graeme Coupar <grambo@grambo.me.uk>
2012-09-13 17:50:23 +0100

    Updated unit tests to use gtest framework
    Previously the unit tests didn't use an actual testing framework, they
    just did some basic manual testing.  This commit updates to use the
    gtest framework.

--- test/cppshouldtest.cpp
+++ test/cppshouldtest.cpp
@@ -2,6 +2,7 @@
 #include "cppshould/expectations/contain.hpp"
 #include "cppshould/operators.hpp"
 #include "cppshould/macros.hpp"
+#include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
 
@@ -9,25 +10,32 @@ using namespace cppshould::expectations;
 
 int main( int argc, char** argv )
 {
+    testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
+}
+
+TEST(ContainTest,BasicTest)
+{
     std::vector< int > intList = { 1, 2, 3 };
     intList SHOULD Contain(1);
     intList >> cppshould::ShouldInfo(__FILE__, __LINE__) << Contain(1);
+    intList SHOULD_NOT Contain(10);
+    
+    // TODO: Replace these with assert_throws or whatever
     try
     {
         intList SHOULD Contain(10);
-        std::cout << "no exception";
+        ASSERT_TRUE( false );
     }
     catch( std::runtime_error& )
     {
-        std::cout << "Caught Expected Exception #1";
     }
-    intList SHOULD_NOT Contain(10);
     try
     {
         intList SHOULD_NOT Contain(1);
+        ASSERT_TRUE( false );
     }
     catch( std::runtime_error& )
     {
-        std::cout << "Caught Expected Exception #2";
     }
 }
