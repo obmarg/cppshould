2eac686382e6ee646eca3429d310ee5f1d81d7b2
Graeme Coupar <grambo@grambo.me.uk>
2012-09-16 15:34:27 +0100

    Implemented BeApprox expectation.
    Currently this expectation just uses plain operators rather than
    specializable classes.  Should do the job for now, but might want to
    update it at a later date...

--- include/cppshould/expectations/be.hpp
+++ include/cppshould/expectations/be.hpp
@@ -4,8 +4,8 @@
 //!
 //! The be expectations should be used to determine whether a value meets
 //! a certain equality or comparison expectation.
-//! 
-//! .. function:: Be( ExpectedT )
+//!
+//! .. function:: Be( ExpectedT expected )
 //!
 //!     Creates a be expectation.  This expectation determines whether
 //!     a value matches another value or not.
@@ -14,7 +14,10 @@
 //!               link to a macro section or something (so we don't miss out
 //!               MUST etc.)
 //!
-//!     Meant to be used with :c:macro:`SHOULD` and :c:macro:`SHOULD_NOT` 
+//!     .. todo:: Mention that comparison is done with EquivalenceTraits
+//!               (which falls back on ==)
+//!
+//!     Meant to be used with :c:macro:`SHOULD` and :c:macro:`SHOULD_NOT`
 //!     For example::
 //!
 //!         intList SHOULD Be(1);      // Will pass
@@ -24,6 +27,31 @@
 //!
 //!     .. todo:: Link to information on SHOULD_BE & SHOULD_NOT_BE etc.
 //!
+//! .. function:: BeApprox( ExpectedT expected, ExpectedT factor )
+//!
+//!     Creates a be approximately expectation.  This expectation determines
+//!     whether a value is close to another value or not.
+//!
+//!     Unlike func::`Be` this expectation does not rely on traits to do
+//!     it's comparisons, instead relying on ``operator>=`` & ``operator<=``.
+//!     This means that it is only suitable for use with types for which
+//!     these operators are defined, such as integers and floats.  Types
+//!     which define ``operator<`` will also work as the other operators
+//!     can be emulated with this operator.
+//!
+//!     .. todo:: Instead of linking directly to SHOULD & SHOULD_NOT maybe just
+//!               link to a macro section or something (so we don't miss out
+//!               MUST etc.)
+//!
+//!     Meant to be used with :c:macro:`SHOULD` and :c:macro:`SHOULD_NOT`
+//!     For example::
+//!
+//!         intList SHOULD Be(1);      // Will pass
+//!         intList SHOULD Be(4);      // Will fail
+//!         intList SHOULD_NOT Be(4);  // Will pass
+//!         intList SHOULD_NOT Be(1);  // Will fail
+//!
+//!     .. todo:: Link to information on SHOULD_BE & SHOULD_NOT_BE etc.
 
 #ifndef CPPSHOULD_EXPECTATIONS_BE_H_
 #define CPPSHOULD_EXPECTATIONS_BE_H_
@@ -31,6 +59,8 @@
 #include "cppshould/expectations/base.hpp"
 #include "cppshould/traits.hpp"
 #include "cppshould/utils.hpp"
+#include <sstream>
+#include <utility>
 
 namespace cppshould {
 namespace expectations {
@@ -59,6 +89,37 @@ std::string BeExpectation< ExpectedT >::ToString() const
     return "be equal to " + ToStringTraits< ExpectedT >::Convert( m_expect );
 }
 
+//
+// Class that implements BeApprox expectation
+//
+template< class ExpectedT >
+class BeApproxExpectation : public Expectation< ExpectedT >
+{
+public:
+    BeApproxExpectation( ExpectedT expect, ExpectedT factor ) :
+    m_expect( expect ),
+    m_factor( factor )
+    {}
+
+    ExpectedT m_expect;
+    ExpectedT m_factor;
+
+protected:
+    virtual std::string ToString() const;
+};
+
+template< class ExpectedT >
+std::string BeApproxExpectation< ExpectedT >::ToString() const
+{
+    std::ostringstream oss;
+    oss << "be approximately equal to "
+        << ToStringTraits< ExpectedT >::Convert( m_expect )
+        << "(by a factor of "
+        << ToStringTraits< ExpectedT >::Convert( m_factor )
+        << ")";
+    return oss.str();
+}
+
 }   // namespace impl
 
 //
@@ -68,6 +129,19 @@ template< class ExpectedT >
 impl::BeExpectation< ExpectedT > Be( ExpectedT expect )
 {
     return impl::BeExpectation< ExpectedT >(expect);
+}
+
+//
+// Factory function for BeApprox expectations
+//
+template< class ExpectedT >
+impl::BeApproxExpectation< ExpectedT > BeApprox(
+        ExpectedT expect, ExpectedT factor
+        )
+{
+    // TODO: would be nice to check if ExpectedT is valid for
+    //       a be approx expectation here.
+    return impl::BeApproxExpectation< ExpectedT >( expect, factor );
 }
 
 }   // namespace expectations
@@ -81,8 +155,8 @@ struct ExpectationTraits<
     expectations::impl::BeExpectation< ExpectedT >
     >
 {
-    typedef EquivalenceTraits< 
-        typename remove_refcv< ActualT >::type, 
+    typedef EquivalenceTraits<
+        typename remove_refcv< ActualT >::type,
         typename remove_refcv< ExpectedT >::type
         > EquivTraits;
 
@@ -92,6 +166,34 @@ struct ExpectationTraits<
             )
     {
         return EquivTraits::Equivalent( actual, expectation.m_expect );
+    }
+};
+
+//
+// Traits specialization for BeApprox expectation
+//
+template< class ActualT, class ExpectedT >
+struct ExpectationTraits<
+    ActualT,
+    expectations::impl::BeApproxExpectation< ExpectedT >
+    >
+{
+    static bool Matches(
+            const ActualT& actual,
+            const expectations::impl::BeApproxExpectation<
+                ExpectedT
+                >& expectation
+            )
+    {
+        using namespace std::rel_ops;
+        ExpectedT min = expectation.m_expect - expectation.m_factor;
+        ExpectedT max = expectation.m_expect + expectation.m_factor;
+        return actual >= min & actual <= max;
+        // NOTE:
+        //
+        // Could probably switch this to use std::less_equal etc.
+        // which would leave room for specializations again
+        // This'll do for now though
     }
 };
 
--- test/expectations/be.cpp
+++ test/expectations/be.cpp
@@ -8,6 +8,7 @@ using ::testing::InSequence;
 using ::testing::_;
 
 using cppshould::expectations::Be;
+using cppshould::expectations::BeApprox;
 using cppshould::testing::MockCallbacks;
 using cppshould::testing::SetupCallbacks;
 
@@ -29,7 +30,7 @@ TEST( BeExpectation, ShouldWorkWithNumbers )
     2 SHOULD Be(2);
     3 SHOULD Be(3);
     4 SHOULD_NOT Be(1);
-    
+
     // Fails
     5 SHOULD Be(4);
 }
@@ -89,4 +90,82 @@ TEST( BeExpectation, ShouldWorkWithVectors )
     vec1 SHOULD Be(vec2);
 }
 
+// TODO: add error message tests
+
 // TODO: Add a test with a custom object ?
+//
+
+TEST( BeApproxExpectation, ShouldWorkWithIntegers )
+{
+    MockCallbacks callbacks;
+    SetupCallbacks( callbacks );
+
+    // Set expectations
+    {
+        InSequence dummy;
+        EXPECT_CALL( callbacks, Pass() )
+            .Times(5);
+        EXPECT_CALL( callbacks, Fail(_, false) )
+            .Times(2);
+    }
+
+    1 SHOULD BeApprox(1, 1);
+    2 SHOULD BeApprox(1, 1);
+    5 SHOULD BeApprox(7, 2);
+    1 SHOULD BeApprox(2, 1);
+    5 SHOULD_NOT BeApprox(1, 3);
+
+    1 SHOULD_NOT BeApprox(2, 1);
+    1 SHOULD BeApprox(2, 0);
+}
+
+TEST( BeApproxExpectation, ShouldWorkWithFloats )
+{
+    MockCallbacks callbacks;
+    SetupCallbacks( callbacks );
+
+    // Set expectations
+    {
+        InSequence dummy;
+        EXPECT_CALL( callbacks, Pass() )
+            .Times(5);
+        EXPECT_CALL( callbacks, Fail(_, false) )
+            .Times(2);
+    }
+
+    // Passes
+    0.9 SHOULD BeApprox(1.0, 0.1);
+    2.5 SHOULD BeApprox(2.0, 1.0);
+    5.0 SHOULD BeApprox(7.0, 2.0);
+    1.0 SHOULD BeApprox(2.0, 1.0);
+    .05 SHOULD_NOT BeApprox(1.0, 0.5);
+
+    // Fails
+    1.0 SHOULD_NOT BeApprox(2.0, 1.0);
+    1.0 SHOULD BeApprox(2.0, 0.0);
+}
+
+TEST( BeApproxExpectation, ShouldDoInterTypeComparisons )
+{
+    MockCallbacks callbacks;
+    SetupCallbacks( callbacks );
+
+    // Set expectations
+    {
+        InSequence dummy;
+        EXPECT_CALL( callbacks, Pass() )
+            .Times(2);
+        EXPECT_CALL( callbacks, Fail(_, false) )
+            .Times(2);
+    }
+
+    // Passes
+    1 SHOULD BeApprox(1.0, 0.1);
+    2.5 SHOULD BeApprox(2, 1);
+
+    // Fails
+    1 SHOULD_NOT BeApprox(1.2, 0.5);
+    3.1 SHOULD BeApprox(2, 1);
+}
+
+// TODO: Add error message tests
