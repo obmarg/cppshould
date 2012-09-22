e63853617b353be69db2c26b86b5ff2802ab3ebc
Graeme Coupar <grambo@grambo.me.uk>
2012-09-22 14:12:55 +0100

    Added RangeTraits specializations for std containers

--- include/cppshould/impl/rangetraits.hpp
+++ include/cppshould/impl/rangetraits.hpp
@@ -9,16 +9,51 @@
 #ifndef CPPSHOULD_IMPL_RANGETRAITS_H_
 #define CPPSHOULD_IMPL_RANGETRAITS_H_
 
+#include <vector>
+#include <deque>
+#include <list>
+#include <string>
+
 namespace cppshould {
 
 //
 // Vector is a range
 //
-template< class ItemType >
-struct RangeTraits< std::vector< ItemType > >
+template< class ItemType, class Allocator >
+struct RangeTraits< std::vector< ItemType, Allocator > >
 {
     static const bool IsRange = true;
     typedef ItemType ValueT;
+};
+
+//
+// Deque is a range
+//
+template< class ItemType, class Allocator >
+struct RangeTraits< std::deque< ItemType, Allocator > >
+{
+    static const bool IsRange = true;
+    typedef ItemType ValueT;
+};
+
+//
+// List is a range
+//
+template< class ItemType, class Allocator >
+struct RangeTraits< std::list< ItemType, Allocator > >
+{
+    static const bool IsRange = true;
+    typedef ItemType ValueT;
+};
+
+//
+// String is a range
+//
+template< class CharT, class Traits, class Allocator >
+struct RangeTraits< std::basic_string< CharT, Traits, Allocator > >
+{
+    static const bool IsRange = true;
+    typedef CharT ValueT;
 };
 
 }   // namespace cppshould
